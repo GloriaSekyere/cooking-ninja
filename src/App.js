b9e4b24a09bc984ff3989bb807a29269a7b7d7c2
Gloria Sekyere <gloriasekyere@Glorias-MacBook-Pro.local>
2022-03-22 12:10:48 +0000

    create navbar component and nest in app component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import './App.css'
+
+//components
+import Navbar from './components/Navbar'
 
 //pages
 import Home from './pages/home/Home'
@@ -7,10 +9,14 @@ import Create from './pages/create/Create'
 import Recipe from './pages/recipe/Recipe'
 import Search from './pages/search/Search'
 
+//styles
+import './App.css'
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Switch>
           <Route exact path="/">
             <Home />
