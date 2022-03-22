fad005db676d735097e6e3ca0d313e30aacbb8d1
Gloria Sekyere <gloriasekyere@Glorias-MacBook-Pro.local>
2022-03-22 12:06:54 +0000

    Install react-router-dom@5.1, nest pages in App component and setup routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css'
+
+//pages
+import Home from './pages/home/Home'
+import Create from './pages/create/Create'
+import Recipe from './pages/recipe/Recipe'
+import Search from './pages/search/Search'
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+
+          <Route path="/create">
+            <Create />
+          </Route>
+
+          <Route path="/recipe/:id">
+            <Recipe />
+          </Route>
+
+          <Route path="/search">
+            <Search />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
