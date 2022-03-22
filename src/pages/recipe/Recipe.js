8b6e4c25cb7002945ccfc6345406a9cc5268e9e9
Gloria Sekyere <gloriasekyere15@gmail.com>
2022-03-22 13:04:09 +0000

    use useParams to get recipe id from url, and use the url to fetch single recipe in Recipe component

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,8 +1,18 @@
+import { useParams } from 'react-router-dom'
+import { useFetch } from '../../hooks/useFetch'
 import './Recipe.css'
 
 function Recipe() {
+
+  const { id } = useParams()
+  const url = "http://localhost:3000/recipes" + id
+  const { data:recipe, error, isPending } = useFetch(url)
+
   return (
-    <div>Recipe</div>
+    <div>
+      {error && <p className='error'>Error loading recipe...</p>}
+      {isPending && <p className="loading">Loading recipe...</p>}
+    </div>
   )
 }
 
