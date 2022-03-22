3452a246e72dfedb56e718922cea68280b79247c
Gloria Sekyere <gloriasekyere15@gmail.com>
2022-03-22 13:17:59 +0000

    output detail recipe template and style component

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -5,13 +5,21 @@ import './Recipe.css'
 function Recipe() {
 
   const { id } = useParams()
-  const url = "http://localhost:3000/recipes" + id
+  const url = "http://localhost:3000/recipes/" + id
   const { data:recipe, error, isPending } = useFetch(url)
 
   return (
     <div>
       {error && <p className='error'>Error loading recipe...</p>}
       {isPending && <p className="loading">Loading recipe...</p>}
+      {recipe && (<div className='detail-recipe'>
+        <h2>{recipe.title}</h2>
+        <p>Cooking Time: {recipe.cookingTime}</p>
+        <ul>
+          {recipe.ingredients.map(ing => <li key={ing}>{ing}</li>)}
+        </ul>
+        <p>{recipe.method}</p>
+      </div>)}
     </div>
   )
 }
