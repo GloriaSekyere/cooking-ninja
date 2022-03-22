2130fec6e622451c1b1dab39ebff187c00348b46
Gloria Sekyere <gloriasekyere15@gmail.com>
2022-03-22 12:49:28 +0000

    add useFetch hook and fetch recipes in Home component

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,8 +1,21 @@
+import { useFetch } from '../../hooks/useFetch'
 import './Home.css'
 
 function Home() {
+  const { data:recipes, error, isPending } = useFetch("http://localhost:3000/recipes")
+
   return (
-    <div>Home</div>
+    <div>
+      {error && <p className='error'>Error loading recipes...</p>}
+      {isPending && <p className='loading'>Loading recipes...</p>}
+      {recipes && recipes.map(recipe => (
+        <div key={recipe.id} className='recipe-card'>
+          <h2>{recipe.title}</h2>
+          <p>Cooking time: {recipe.cookingTime}</p>
+          <p>{recipe.method.substring(0,99)}...</p>
+        </div>
+      ))}
+    </div>
   )
 }
 
